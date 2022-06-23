8fe5a63e002f9264203cc49c652e47b0f6a709c9
Preeti325 <preetivishnoi37@gmail.com>
2022-06-23 23:37:31 +0530

    Constant File added

---
+++ src/main/java/com/preeti/camel/constants/Constant.java
@@ -0,0 +1,15 @@
+package com.preeti.camel.constants;
+
+import com.preeti.camel.dto.Time;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Constant {
+    public static final List<Time> list = new ArrayList<>();
+    public static final String Name = "Nitu Gupta";
+    public static final String Status1 = "Good Morning"; //12:00AM-12:00PM
+    public static final String Status2 = "Good Afternoon"; //12:00PM-04:00PM
+    public static final String Status3 = "Good Evening"; //04:00PM-09:00PM
+    public static final String Status4 = "Good Night"; //09:00PM-12:00AM
+}
--- src/main/java/com/preeti/camel/service/TimeService.java
+++ src/main/java/com/preeti/camel/service/TimeService.java
@@ -1,31 +1,40 @@
 package com.preeti.camel.service;
 
+import static com.preeti.camel.constants.Constant.Name;
+import static com.preeti.camel.constants.Constant.Status1;
+import static com.preeti.camel.constants.Constant.Status2;
+import static com.preeti.camel.constants.Constant.Status3;
+import static com.preeti.camel.constants.Constant.Status4;
+import static com.preeti.camel.constants.Constant.list;
+
 import com.preeti.camel.dto.Time;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
+import javax.annotation.PreDestroy;
 import java.util.Calendar;
 import java.util.List;
 
 @Service
 public class TimeService {
 
-    private List<Time> list = new ArrayList<>();
-
     @PostConstruct
     public void initDB() {
+    }
+
+    private void status() {
         Calendar c = Calendar.getInstance();
         int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
         if (timeOfDay < 12) {
-            list.add(new Time("Good Morning","Nitu Gupta"));
+            list.add(new Time(Status1, Name));
         } else if (timeOfDay < 16) {
-            list.add(new Time("Good Afternoon", "Nitu Gupta"));
+            list.add(new Time(Status2, Name));
         } else if (timeOfDay < 21) {
-            list.add(new Time("Good Evening", "Nitu Gupta"));
+            list.add(new Time(Status3, Name));
         } else {
-            list.add(new Time("Good Night", "Nitu Gupta"));
+            list.add(new Time(Status4, Name));
         }
+
     }
 
     public void addTimeStatus(Time time) {
@@ -33,6 +42,11 @@ public class TimeService {
     }
 
     public List<Time> getTimeStatus() {
+        status();
         return list;
+    }
+
+    @PreDestroy
+    public void destroy() {
     }
 }
